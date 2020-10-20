1088ae88ed6d5563ff9603f736b7acb82c676239
Fibonacci <dave.green1664@yahoo.co.uk>
2020-10-20 22:08:38 +0100

    Implemented an insertionSort algo utilizing a insertOrdered function. Testing algo with various amount of int data printing out to CSV files, also ran all the same tests using vector instead of list.

--- sorting/main.cpp
+++ sorting/main.cpp
@@ -11,6 +11,7 @@
 #include <list>
 #include <vector>
 
+
 // Import things we need from the standard library
 using std::chrono::duration_cast;
 using std::chrono::milliseconds;
@@ -27,7 +28,8 @@ using std::vector;
 typedef std::chrono::steady_clock the_clock;
 
 float sum = 0;
-ofstream intSortingTimes("intSortingTimes.csv");
+//ofstream intSortingTimes("intSortingTimes.csv");
+ofstream vectorSortingTimes("vectorSortingTimes.csv");
 
 
 // Put "count" random integers in the range (0, 99) into "collection".
@@ -80,56 +82,225 @@ void AssertSorted(const T& collection)
 	// We've got to the end of the list. So it's sorted!
 }
 
-int main(int argc, char *argv[])
+template <typename Col, typename Item>
+void insertOrdered(Col& collection, Item& item)
 {
-	srand((unsigned int) time(NULL));		// Seed the rng.
+	auto it = collection.begin();
+
+	while (it != collection.end())
+	{
+		if (*it > item)
+		{
+			// Found the item.
+			collection.insert(it, item);
+			return;
+		}
+
+		++it;
+	}
+
+	// Never found place to insert, add item to end of list.
+	collection.push_back(item);
+}
+
+template <typename T>
+T insertionSort(T collection)
+{
+	T output;
+
+	for (auto& item : collection)
+	{
+		insertOrdered(output, item);
+	}
+
+	return output;
+}
+
+void implementingSorting()
+{
+	int num_values = 100;
+
+	cout << "Number of Items\t\tTime 1\t\tTime 2\t\tTime 3\n\n";
+	//moreSortingTimes << "Number of Items, Time 1, Time 2, Time 3\n";
+
+	while (num_values <= 2000)
+	{
+		cout << num_values << "\t\t\t";
+		//moreSortingTimes << num_values << ",";
+
+		// Sort each number of items 3 times before incrementing by another 10,000;
+		for (int i = 0; i < 3; ++i)
+		{
+			list<int> input;
+			MakeRandomValues(input, num_values);
+
+			// Start timing
+			the_clock::time_point start = the_clock::now();
+			// Sort the list.
+			list<int> output = insertionSort(input);
+			// Stop timing
+			the_clock::time_point end = the_clock::now();
+
+			// Compute the difference between the two times in milliseconds
+			auto time_taken = duration_cast<milliseconds>(end - start).count();
+			//cout << "Time taken to sort " << num_values << " intergers = " << time_taken << " ms.\n\n";
+
+			cout << time_taken << "ms\t\t";
+			//moreSortingTimes << time_taken << ", ";
+
+			if (i == 2)
+			{
+				cout << '\n';
+				//moreSortingTimes << ",\n";
+			}
+
+			//cout << "After sorting: ";
+			//ShowValues(output);
+			//AssertSorted(output);
+		}
+
+		// Increment by 10,000
+		num_values += 100;
+	}
+
+	// Add values.
+	/*shopping.push_back("Milk");
+	shopping.push_back("Bread");
+	shopping.push_back("Cookies");
+	shopping.push_back("Sausages");
+	shopping.push_back("Coffee");
+	shopping.push_back("Sugar");
+	shopping.push_back("Tea");*/
+
+	//ShowValues(nums);
+	//shopping.sort();
+
+	//list<int> sorted = insertionSort(nums);
+
+	//ShowValues(shopping);
+
+	//AssertSorted(orderedShopping);
+
+	/*insertOrdered(sorted, "Apples");
+	insertOrdered(sorted, "Brisket");
+	insertOrdered(sorted, "Peas");
+	insertOrdered(sorted, "Yoghurts");*/
+
+	//ShowValues(sorted);
+	//AssertSorted(shopping);
+}
+
+void implementingSortingVector()
+{
+	int num_values = 100;
+
+	cout << "Number of Items\t\tTime 1\t\tTime 2\t\tTime 3\n\n";
+	vectorSortingTimes << "Number of Items, Time 1, Time 2, Time 3\n";
+
+	while (num_values <= 2000)
+	{
+		cout << num_values << "\t\t\t";
+		vectorSortingTimes << num_values << ",";
+
+		// Sort each number of items 3 times before incrementing by another 10,000;
+		for (int i = 0; i < 3; ++i)
+		{
+			vector<int> input;
+			MakeRandomValues(input, num_values);
+
+			// Start timing
+			the_clock::time_point start = the_clock::now();
+			// Sort the list.
+			vector<int> output = insertionSort(input);
+			// Stop timing
+			the_clock::time_point end = the_clock::now();
+
+			// Compute the difference between the two times in milliseconds
+			auto time_taken = duration_cast<milliseconds>(end - start).count();
+			//cout << "Time taken to sort " << num_values << " intergers = " << time_taken << " ms.\n\n";
+
+			cout << time_taken << "ms\t\t";
+			vectorSortingTimes << time_taken << ", ";
+
+			if (i == 2)
+			{
+				cout << '\n';
+				vectorSortingTimes << ",\n";
+			}
+
+			//cout << "After sorting: ";
+			//ShowValues(output);
+			//AssertSorted(output);
+		}
+
+		// Increment by 10,000
+		num_values += 100;
+	}
+}
+
+void writingToCSV()
+{
+	srand((unsigned int)time(NULL));		// Seed the rng.
 
 	int num_values = 10000;
 
-	cout << "Number of Items\t\t\t\t\tTime to Sort\n\n";
-	intSortingTimes << "Number of Items, Time to Sort\n";
+	cout << "Number of Items\t\tTime 1\t\tTime 2\t\tTime 3\n\n";
+	vectorSortingTimes << "Number of Items, Time 1, Time 2, Time 3\n";
 
 	while (num_values <= 100000)
 	{
 		//cout << "Sorting " << num_values << " integers\n";
 
-		list<int> input;
-		MakeRandomValues(input, num_values);
-		//cout << "Before sorting: ";
-		//ShowValues(input);
+		cout << num_values << "\t\t\t";
+		vectorSortingTimes << num_values << ",";
 
-		// Sort the list.
-		list<int> output = input;
+		// Sort each number of items 3 times before incrementing by another 10,000;
+		for (int i = 0; i < 3; ++i)
+		{
+			list<int> input;
+			MakeRandomValues(input, num_values);
+			//cout << "Before sorting: ";
+			//ShowValues(input);
 
-		// Start timing
-		the_clock::time_point start = the_clock::now();
-		output.sort();
-		// Stop timing
-		the_clock::time_point end = the_clock::now();
+			// Sort the list.
+			list<int> output = input;
 
-		// Compute the difference between the two times in milliseconds
-		auto time_taken = duration_cast<milliseconds>(end - start).count();
-		//cout << "Time taken to sort " << num_values << " intergers = " << time_taken << " ms.\n\n";
+			// Start timing
+			the_clock::time_point start = the_clock::now();
+			output.sort();
+			// Stop timing
+			the_clock::time_point end = the_clock::now();
 
-		cout << num_values << "\t\t\t\t\t\t" << time_taken << "ms\n\n";
-		intSortingTimes << num_values << ", " << time_taken << "ms\n";
+			// Compute the difference between the two times in milliseconds
+			auto time_taken = duration_cast<milliseconds>(end - start).count();
+			//cout << "Time taken to sort " << num_values << " intergers = " << time_taken << " ms.\n\n";
 
-		//cout << "After sorting: ";
-		//ShowValues(output);
-		AssertSorted(output);
+			cout << time_taken << "ms\t\t";
+			vectorSortingTimes << time_taken << ", ";
 
+			if (i == 2)
+			{
+				cout << '\n';
+				vectorSortingTimes << ",\n";
+			}
+
+			//cout << "After sorting: ";
+			//ShowValues(output);
+			AssertSorted(output);
+		}
+
+		// Increment by 10,000
 		num_values += 10000;
 	}
+}
+
+int main(int argc, char *argv[])
+{
+	//writingToCSV();
+	//implementingSorting();
+	implementingSortingVector();
 
 	cout << "All OK!\n";
 
 	return 0;
 }
-
-
-
-
-
-
-
-
